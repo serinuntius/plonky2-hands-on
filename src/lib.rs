237f712c01008d7add14892f848a7e3657307934
serinuntius <wasab1gj@gmail.com>
2023-06-10 11:28:28 +0900

    recursive proof

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,15 @@
 mod tests {
     use plonky2::{
         field::{goldilocks_field::GoldilocksField, types::Field},
-        iop::witness::{PartialWitness, WitnessWrite},
+        iop::{
+            target::Target,
+            witness::{PartialWitness, WitnessWrite},
+        },
         plonk::{
-            circuit_builder::CircuitBuilder, circuit_data::CircuitConfig,
+            circuit_builder::CircuitBuilder,
+            circuit_data::{CircuitConfig, CircuitData},
             config::PoseidonGoldilocksConfig,
+            proof::ProofWithPublicInputs,
         },
     };
 
@@ -51,6 +56,66 @@ mod tests {
         let proof = data.prove(pw).unwrap();
 
         // 検証
+        data.verify(proof).unwrap();
+    }
+
+    struct InnerTarget {
+        a: Target,
+        b: Target,
+        c: Target,
+    }
+
+    // inner circuit(再起証明の対象になる回路)を生成する関数
+    fn build_inner_circuit() -> (CircuitData<F, C, D>, InnerTarget) {
+        let config = CircuitConfig::standard_recursion_config();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+
+        let one = F::from_canonical_u64(1);
+        let two = F::from_canonical_u64(2);
+        let a = builder.add_virtual_target();
+        let b = builder.add_virtual_target();
+
+        let c = builder.add(a, b);
+
+        let data = builder.build::<C>();
+        let target = InnerTarget { a, b, c };
+        (data, target)
+    }
+
+    fn generate_inner_proof(
+        data: &CircuitData<F, C, D>,
+        it: &InnerTarget,
+    ) -> ProofWithPublicInputs<F, C, D> {
+        let mut pw = PartialWitness::new();
+        pw.set_target(it.a, F::from_canonical_u64(1));
+        pw.set_target(it.b, F::from_canonical_u64(2));
+        pw.set_target(it.c, F::from_canonical_u64(3));
+
+        data.prove(pw).unwrap()
+    }
+
+    #[test]
+    fn test_recursive_proof() {
+        let (inner_data, inner_target) = build_inner_circuit();
+
+        let config = CircuitConfig::standard_recursion_config();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+
+        let inner_verifier_data = builder.constant_verifier_data(&inner_data.verifier_only);
+        let proof_with_pis = builder.add_virtual_proof_with_pis(&inner_data.common);
+
+        let inner_proof = generate_inner_proof(&inner_data, &inner_target);
+
+        // recursion proof
+        builder.verify_proof::<C>(&proof_with_pis, &inner_verifier_data, &inner_data.common);
+
+        let mut pw = PartialWitness::<F>::new();
+
+        pw.set_proof_with_pis_target(&proof_with_pis, &inner_proof);
+
+        let data = builder.build::<C>();
+        let proof = data.prove(pw).unwrap();
+
         data.verify(proof).unwrap();
     }
 }
